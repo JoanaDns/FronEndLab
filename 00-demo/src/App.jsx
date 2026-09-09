374a588ce580f96d7239a0778184d9e7bf8366fb
JOANA ISADORA DUTRA NUNES <joana_i_nunes@estudante.sesisenai.org.br>
2026-09-09 17:09:30 -0300

    commit feito

---
+++ 00-demo/src/App.jsx
@@ -0,0 +1,37 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="cont-app">
+      <h1>Trabalhando com React</h1>
+      <h2>Onde posso Trabalhar como dev</h2>
+      <p1>Empresas e Software Houses: Consultorias e fábricas de software que desenvolvem aplicações 
+      sob demanda para terceiros.</p1>
+      <p2>Agências Digitais e de Marketing: Criação e manutenção de e-commerces, landing pages interativas,
+       hotsites e blogs corporativos.</p2>
+      <p3>Assistente / Analista de Suporte a Software: Posição inicial estratégica para entender a regra de 
+      negócio da empresa enquanto realiza pequenas manutenções no código.</p3>
+
+      <h2>trabalho com tecnologia mas sem programação</h2>
+    <p>
+      Gostar de tecnologia sem ter afinidade com código não é um obstáculo. O mercado de TI é 
+      gigante e depende fortemente de funções que focam em pessoas, processos, design, dados e negócio.
+    </p>
+    <ul>
+      <li>Design e Experiência do Usuário (UI/UX)</li>
+      <li>Gestão de Projetos e Agilidade</li>
+      <li>Dados e Inteligência de Negócio</li>
+      <li>Garantia de Qualidade e Suporte</li>
+      <li>Vendas Técnicas e Sucesso do Cliente</li>
+    </ul>
+    
+    </div>
+
+
+  )
+}
+
+export default App
